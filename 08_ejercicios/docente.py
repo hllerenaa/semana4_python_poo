61575d0f4598c5f8c4e676b8f56f2dfbf938bcaf
hllerenaa <hllerenaa1h@gmail.com>
2026-06-04 20:41:03 -0500

    CLASE

--- 08_ejercicios/docente.py
+++ 08_ejercicios/docente.py
@@ -16,3 +16,28 @@ llamada_dispositivo = Dispositivo("10.0.0.1", "AA:BB:CC:DD:EE:FF")
 llamada_dispositivo.reportar()
 es_valida_ = llamada_dispositivo.es_valida()
 print(es_valida_)
+
+
+"""
+RETO: Clase Router
+
+Atributos (en __init__):
+  - ip
+  - modelo
+  - rutas_configuradas (empieza en 0)
+
+Metodos:
+  - agregar_ruta(cantidad)  ->  suma cantidad a rutas_configuradas
+  - reportar()              ->  imprime ip, modelo, rutas_configuradas
+
+Pruebas al final:
+  - Crear un router 192.168.1.1, Cisco-2960
+  - agregar 2 rutas
+  - agregar 4 rutas
+  - agregar 1 ruta
+  - llamar reportar()
+
+Salida esperada:
+  IP: 192.168.1.1, Modelo: Cisco-2960, Rutas: 7
+
+"""
